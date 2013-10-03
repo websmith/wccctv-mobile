949ea239135bd86630cdf61095c89a72cf087bb8
Nate Cornelius <nate@holeintheroof.com>
2013-10-03 17:08:07 -0500

    update javascript to jquery, live player updated to use iframe

--- js/vimeo.js
+++ js/vimeo.js
@@ -1,83 +1,52 @@
-var vimeoUserName = 'wccctv';
-var vimeoChannelName = '380729';
+var vimeoUserName = 'wccctv',
+	vimeoChannelName = '380729',
+	callback = 'showThumbs',
+	url = 'http://vimeo.com/api/v2/channel/' + vimeoChannelName + '/videos.json?callback=' + callback;
 
-// Tell Vimeo what function to call
-var callback = 'showThumbs';
-
-// Set up the URLs
-var url = 'http://vimeo.com/api/v2/channel/' + vimeoChannelName + '/videos.json?callback=' + callback;
-
-// This function loads the data from Vimeo
 function init() {
-    var js = document.createElement('script');
-    js.setAttribute('src', url);
-	document.getElementsByTagName('head').item(0).appendChild(js);
+	$('head').append('<script src="'+url+'"></script>');
 }
 
-// This function goes through the clips and puts them on the page
+// Iterates through the videos and adds them to the page
 function showThumbs(videos) {
-	var thumbs = document.getElementById('thumbs');
-	thumbs.innerHTML = '';
-    
-    //Convert duration to min:sec format
-    function convertTime(duration) { 
-        var v_minutes = parseInt(duration / 60, 10) % 60;
-        var v_seconds = duration % 60;
-        var v_playtime = (v_minutes < 10 ? "0" + v_minutes : v_minutes) + ":" + (v_seconds < 10 ? "0" + v_seconds : v_seconds);
-        return v_playtime;
-    }
-    
-    //strip quotes from strings
-    function stripQuotes(str) {
-        return (str=str.replace('<br/>',''));
-    }
+	var thumbs = $('#thumbs');
+	$(thumbs).html('');
+	
+	//Convert duration to min:sec format
+	function convertTime(duration) { 
+		var v_minutes = parseInt(duration / 60, 10) % 60;
+		var v_seconds = duration % 60;
+		var v_playtime = (v_minutes < 10 ? "0" + v_minutes : v_minutes) + ":" + (v_seconds < 10 ? "0" + v_seconds : v_seconds);
+		return v_playtime;
+	}
+	
+	//strip quotes from strings
+	function stripQuotes(str) {
+		return (str=str.replace('<br/>',''));
+	}
   
 	for (var i = 0; i < videos.length; i++) {
-        var thumb = document.createElement('img');
-        var wrap = document.createElement('div');
-        var title = document.createElement('h1');
-        var duration = document.createElement('small');
-        var clear1 = document.createElement('div');
-        var desc = document.createElement('p');
-        var clear2 = document.createElement('div');
-        var linkbox = document.createElement('a');
-        var li = document.createElement('li');
-        
-        wrap.setAttribute('class', 'wrap');
-        title.appendChild(document.createTextNode(videos[i].title));
-        title.setAttribute('class', 'thumb-title');
-        duration.appendChild(document.createTextNode(convertTime(videos[i].duration)));
-        duration.setAttribute('class', 'thumb-duration');
-        clear1.setAttribute('class', 'cl');
-        desc.appendChild(document.createTextNode(stripQuotes(videos[i].description)));
-        desc.setAttribute('class', 'thumb-desc');
-        clear2.setAttribute('class', 'cl');
-        linkbox.setAttribute('href', '#video?id=' + videos[i].id);
-        linkbox.setAttribute('class', 'ui-link');
-        linkbox.setAttribute('id', i);
-		thumbs.appendChild(linkbox);
-        
-		thumb.setAttribute('src', videos[i].thumbnail_medium);
-		thumb.setAttribute('alt', videos[i].title);
-		thumb.setAttribute('title', videos[i].title);
-        thumb.setAttribute('class', 'thumb-img');
-				
-        wrap.appendChild(thumb);
-		wrap.appendChild(title);
-		wrap.appendChild(duration);
-        wrap.appendChild(clear1);
-		thumbs.appendChild(li);
-		li.appendChild(linkbox);
-		linkbox.appendChild(wrap);
-		linkbox.appendChild(desc);
-		linkbox.appendChild(clear2);
-    }
+		$(thumbs).append(
+			'<li>'+
+				'<a href="#video?id=' + videos[i].id + '" class="ui-link" id="'+i+'">'+
+					'<div class="wrap">'+
+						'<img src="'+videos[i].thumbnail_medium+'" alt="'+videos[i].title+'" title="'+videos[i].title+'" class="thumb-img">'+
+						'<h1 class="thumb-title">'+videos[i].title+'</h1>'+
+						'<small class="thumb-duration">'+convertTime(videos[i].duration)+'</small>'+
+						'<div class="cl"></div>'+
+					'</div>'+
+					'<p class="thumb-desc">'+stripQuotes(videos[i].description)+'</p>'+
+					'<div class="cl"></div>'+
+				'</a>'+
+			'</li>'
+		);
+	}
 
-    $('.ui-link').on('click', function() {
-        //return the parameter as a variable
-        var param = this.href.split('=')[1];
+	$('.ui-link').on('click', function() {
+		//return the parameter as a variable
+		var param = this.href.split('=')[1];
 
-		/* Poplate Video */
+		/* Poplate Video and info */
 		$('#vimeo').html("").append(
 		'<div class="wrap">'+
 		'<iframe src="http://player.vimeo.com/video/'+ videos[this.id].id +'?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=0&amp;color=ffffff" class="vimeo-iframe" color="ffffff" width="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'+
@@ -93,10 +62,18 @@ function showThumbs(videos) {
 		$(document).ready(function(){
 			var ratio = 2.1;
 			var outerwidth = $('#video').width();
+			var livewidth = $('#live-player').width();
+			console.log(livewidth);
 			$('.vimeo-iframe').attr("height", outerwidth/ratio);
+			$('#live-player').attr("height", livewidth/ratio);
 		});
 	});  
 }
 
-// Call init function when the page loads
+$(document).on('pageshow', '#live',  function(){
+	var ratio = 2.1;
+	var livewidth = $('#live').width();
+	$('#live-player').attr("height", livewidth/ratio);
+});
+
 window.onload = init;
